22c57f37c816f7d5a17181232b838f4b796d3155
spyron1 <divyanshuaarayan@gmail.com>
2025-04-07 21:09:37 +0530

    all

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Divyanshu Aaryan',
+    description= 'A simple medical chat bot',
+    author_email= 'divyanshuaarayan@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
